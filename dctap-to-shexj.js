071ec109a709d213bfa3f01e6f887d73c122843d
Eric Prud'hommeaux <eric+github@w3.org>
2021-07-25 06:45:05 +0200

    ~ un-nest functions

--- dctap-to-shexj.js
+++ dctap-to-shexj.js
@@ -41,124 +41,109 @@ function dctapToShExJ (dctap, base) {
     row.valueNodeType = row.valueNodeType.toLowerCase()
     row.valueConstraintType = row.valueConstraintType.toLowerCase()
 
-    const {
-      shapeID,
-      shapeLabel,
-      propertyID,
-      propertyLabel,
-      mandatory,
-      repeatable,
-      valueNodeType,
-      valueDataType,
-      valueConstraint,
-      valueConstraintType,
-      valueShape,
-      note,
-    } = row
-
-    if (shapeID) {
+    if (row.shapeID) {
       if (curShape) {
         curShape.expression = maybeAnd(conjuncts, "EachOf", "expressions")
       }
       conjuncts = []
       curShape = {
         type: "Shape",
-        id: new URL(shapeID, base).href,
+        id: new URL(row.shapeID, base).href,
       }
       schema.shapes.push(curShape)
     } else if (!curShape) {
       throw new Error(`no current shape into which to add ${JSON.stringify(row)}`)
     }
-    conjuncts.push(toTC(row))
+    conjuncts.push(toTC(row, base))
   })
   if (curShape) {
     curShape.expression = maybeAnd(conjuncts, "EachOf", "expressions")
   }
   return schema
+}
 
-  function toTC (sc) {
+
+function toTC (sc, base) {
+  return {
+    type: "TripleConstraint",
+    predicate: sc.propertyID,
+    valueExpr: parseExpr(sc, base)
+  }
+}
+
+function parseExpr (sc, base) {
+  const valueExprs = []
+
+  switch (sc.valueConstraintType) {
+  case "iristem":
+  case "picklist":
+  case "languagetag":
+    const values = sc.valueConstraint.split(/\s+/)
+    valueExprs.push({
+      type: "NodeConstraint",
+      values: values.map(v => coerseV(v, sc, sc.valueConstraintType.endsWith('stem')))
+    })
+    break
+  case "pattern":
+    valueExprs.push({
+      type: "NodeConstraint",
+      pattern: sc.valueConstraint
+    })
+    break
+  case "":
+    break
+  default: throw Error(`What's a valueConstraintType ${sc.valueConstraintType} in ${JSON.stringify(sc, null, 2)}?`)
+  }
+  if (sc.valueShape)
+    valueExprs.push(new URL(sc.valueShape, base).href)
+  return maybeAnd(valueExprs, "ShapeAnd", "shapeExprs")
+}
+
+function coerseV (v, sc, isStem = false) {
+  if (sc.valueConstraintType === "languagetag")
     return {
-      type: "TripleConstraint",
-      predicate: sc.propertyID,
-      valueExpr: parseExpr(sc)
+      type: "Language",
+      languageTag: v
     }
-  }
 
-  function parseExpr (sc) {
-    const valueExprs = []
-
-    switch (sc.valueConstraintType) {
-    case "iristem":
-    case "picklist":
-    case "languagetag":
-      const values = sc.valueConstraint.split(/\s+/)
-      valueExprs.push({
-        type: "NodeConstraint",
-        values: values.map(v => coerseV(v, sc, sc.valueConstraintType.endsWith('stem')))
-      })
-      break
-    case "pattern":
-      valueExprs.push({
-        type: "NodeConstraint",
-        pattern: sc.valueConstraint
-      })
-      break
-    case "":
-      break
-    default: throw Error(`What's a valueConstraintType ${sc.valueConstraintType} in ${JSON.stringify(sc, null, 2)}?`)
-    }
-    if (sc.valueShape)
-      valueExprs.push(new URL(sc.valueShape, base).href)
-    return maybeAnd(valueExprs, "ShapeAnd", "shapeExprs")
-  }
-
-  function coerseV (v, sc, isStem = false) {
-    if (sc.valueConstraintType === "languagetag")
-      return {
-        type: "Language",
-        languageTag: v
-      }
-
-    switch (sc.valueNodeType) {
-    case "literal":
-      const ret = isStem
-        ? {
-          type: "LiteralStem",
-          value: v
-        }
-      : {
+  switch (sc.valueNodeType) {
+  case "literal":
+    const ret = isStem
+      ? {
+        type: "LiteralStem",
         value: v
       }
-      if (sc.valueDataType && sc.valueDataType !== "xsd:string")
-        ret.datatype = sc.valueDataType
-      return ret
-    case "iri":
-      return isStem
-        ? {
-          type: "IriStem",
-          stem: v
-        }
-      : v
-    case "":
-      return {
-        value: v
-      }
-    default: throw Error(`What's a valueNodeType=${sc.valueNodeType} in ${JSON.stringify(sc, null, 2)}?`)
+    : {
+      value: v
     }
+    if (sc.valueDataType && sc.valueDataType !== "xsd:string")
+      ret.datatype = sc.valueDataType
+    return ret
+  case "iri":
+    return isStem
+      ? {
+        type: "IriStem",
+        stem: v
+      }
+    : v
+  case "":
+    return {
+      value: v
+    }
+  default: throw Error(`What's a valueNodeType=${sc.valueNodeType} in ${JSON.stringify(sc, null, 2)}?`)
   }
+}
 
-  function maybeAnd (conjuncts, type, property) {
-    if (conjuncts.length === 0)
-      return  undefined
+function maybeAnd (conjuncts, type, property) {
+  if (conjuncts.length === 0)
+    return  undefined
 
-    if (conjuncts.length === 1)
+  if (conjuncts.length === 1)
     return conjuncts[0]
 
-    const ret = { type }
-    ret[property] = conjuncts
-    return ret
-  }
-
+  const ret = { type }
+  ret[property] = conjuncts
+  return ret
 }
 
 module.exports = { DcTapToShExJ }
